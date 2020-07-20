233b5978850335b514a4a490b32e5c80e9c0b076
yoojinhee03 <yoojinhee03@gmail.com>
2020-07-20 15:30:47 +0900

    캡쳐 기능 수정

--- js/capture.js
+++ js/capture.js
@@ -1,11 +1,11 @@
 function partShot() {
         
-    var background = document.getElementById('body').style.background;
-    if(background == "") {
-        // document.getElementById('model').style.background = "#fff"; 
-    }
+    // var background = document.getElementById('model').style.background;
+    // if(background == "") {
+    //     // document.getElementById('model').style.background = "#fff"; 
+    // }
         
-    html2canvas(document.getElementById('body'), {
+    html2canvas(document.getElementById('model'), {
         useCORS: true, // 다른사이트의리소스가있을때활성화(그러나...Access-Control-Allow-Origin 필요)
         onrendered: function(canvas) {
             canvas.toBlob(function(blob) {
