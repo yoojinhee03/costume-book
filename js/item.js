d3aba695921ff33d4cc074322b6607fdb3a074a9
yoojinhee03 <yoojinhee03@gmail.com>
2020-07-21 12:56:59 +0900

    makeUp 기능 추가

--- js/item.js
+++ js/item.js
@@ -2,20 +2,27 @@
 function changeItem(id,idx){
 	 let item=document.getElementsByClassName("row")[idx].children;
      let items = document.getElementById("item-container").children;//2
-     // console.log(totalItem);
-
      let complete = document.getElementsByClassName("complete");//2
 
-	 for(let i = 0; i < item.length; i++){
+     for(let i = 0; i < item.length; i++){
     	item[i].style.display="inline-block";
-    }
-    for(let i=0; i<items.length; i++){//2
+     }
+     for(let i=0; i<items.length; i++){//2
     	if(i==idx)continue;
     	for(let j=0; j<items[i].children.length; j++){
     		items[i].children[j].style.display="none";
     	}
-    }
+     }
     // for(let i=0; i<complete.length; i++){
     // 	complete[i].style.display="inline-block";
     // }
+}
+
+function changeMakeUp(item){
+    console.log(item);
+    var imageSrc=item.src.split("icon/");
+    var id=item.id;
+    document.getElementById(id+"Img").src="images/makeUp/"+id+"/"+imageSrc[1];
+    // console.log(imageSrc);
+    // alert(item);
 }
